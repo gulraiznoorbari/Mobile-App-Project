03825f9648585ba425b56b63f2fb4f6442d5939f
gulraiz <gulraiznoorbari@gmail.com>
2023-05-22 23:56:52 +0500

    📦 NEW: Initialized Firebase

---
+++ Code/Travely_App/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { FIREBASE_API_KEY, FIREBASE_APP_ID } from "@env";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: FIREBASE_API_KEY,
+    authDomain: "mobile-app-project-cscs468.firebaseapp.com",
+    projectId: "mobile-app-project-cscs468",
+    storageBucket: "mobile-app-project-cscs468.appspot.com",
+    messagingSenderId: "34661368879",
+    appId: FIREBASE_APP_ID,
+    measurementId: "G-SX1CDS57VV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
