4a55e9695a2d2276bb2230ec16a9a8fdb7418557
gulraiz <gulraiznoorbari@gmail.com>
2023-05-23 12:04:40 +0500

    🐛 FIX: Log out and redirect to Login screen

--- Code/Travely_App/screens/Home.js
+++ Code/Travely_App/screens/Home.js
@@ -1,13 +1,32 @@
 import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
 
 import { auth } from "../firebase/config";
+import { signOut } from "firebase/auth";
 
-const Home = () => {
+const Home = ({ setUserLoggedIn }) => {
+    const [errMessage, setErrMessage] = useState("");
+
+    const handleLogout = () => {
+        signOut(auth)
+            .then(() => {
+                console.log("User signed out!");
+                setUserLoggedIn(false);
+            })
+            .catch((error) => {
+                console.log(error.message);
+                setErrMessage(error.message);
+            });
+    };
+
     return (
         <View style={styles.container}>
             <Text style={styles.heading}>Home</Text>
             <View style={styles.buttonContainer}>
-                <Button title="Logout" onPress={() => auth.signOut()} />
+                <Button title="Logout" onPress={handleLogout} />
+            </View>
+            <View style={styles.errorContainer}>
+                <Text style={styles.errorText}>{errMessage}</Text>
             </View>
         </View>
     );
@@ -25,6 +44,12 @@ const styles = StyleSheet.create({
     },
     buttonContainer: {
         marginTop: 20,
+    },
+    errorText: {
+        color: "red",
+    },
+    errorContainer: {
+        height: 20,
     },
 });
 
