2877090023a634d74e46ea769743fc997da8e806
gulraiz <gulraiznoorbari@gmail.com>
2023-05-30 20:42:51 +0500

    👌 IMPROVE: Auto change the users device status bar color

--- Code/Travely_App/App.js
+++ Code/Travely_App/App.js
@@ -1,18 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
-import HomeScreen from "./screens/HomeScreen";
+import PasswordResetScreen from "./screens/PasswordResetScreen";
 
 const Stack = createStackNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
+            <StatusBar barStyle="light-content" backgroundColor="#003580" />
             <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="SignUp" component={SignUpScreen} />
+                <Stack.Screen name="PasswordReset" component={PasswordResetScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
