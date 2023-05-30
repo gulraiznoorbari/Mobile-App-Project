6054ccacb5eadd299c13095ba07ec7b01fc5bda5
gulraiz <gulraiznoorbari@gmail.com>
2023-05-30 20:40:37 +0500

    👌 IMPROVE: Updated Login Screen UI

--- Code/Travely_App/screens/LoginScreen.js
+++ Code/Travely_App/screens/LoginScreen.js
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react";
-import { useNavigation, Link } from "@react-navigation/native";
-import { StyleSheet, Text, TextInput, View, Button, KeyboardAvoidingView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { StyleSheet, Text, View, KeyboardAvoidingView } from "react-native";
 import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 
 import { authentication } from "../firebase/config";
 import GoogleAuth from "../components/GoogleAuth";
+import LogoHeader from "../components/LogoHeader";
+import SubHeading from "../components/SubHeading";
+import InputField from "../components/InputField";
+import ErrorMessage from "../components/ErrorMessage";
+import PrimaryButton from "../components/PrimaryButton";
+import TextLink from "../components/TextLink";
+import TextLinkIcon from "../components/TextLinkIcon";
+import UserAgreement from "../components/UserAgreement";
 
 const LoginScreen = () => {
     const [email, setEmail] = useState("");
@@ -53,100 +62,68 @@ const LoginScreen = () => {
     };
 
     return (
-        <KeyboardAvoidingView behavior="padding">
-            <View style={styles.logoContainer}>
-                <Text style={styles.logoHeading}>Travely.</Text>
-            </View>
-            <View style={styles.subHeadingContainer}>
-                <Text style={styles.subHeading}>Sign in to unlock best of Travely</Text>
-            </View>
-            <TextInput
-                placeholder="Enter Email"
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <LogoHeader text="Travely." />
+            <SubHeading text="Sign in to unlock the best of Travely." />
+            <InputField
+                label="Email address"
+                placeholder="Enter Email Address"
                 value={email}
-                onChangeText={setEmail}
-                keyboardType="email-address"
-                style={styles.email}
+                setValue={setEmail}
             />
-            <TextInput
+            <InputField
+                label="Password"
                 placeholder="Enter Password"
                 value={password}
-                onChangeText={setPassword}
-                keyboardType="default"
-                secureTextEntry={true}
-                style={styles.password}
+                setValue={setPassword}
             />
-            <View style={styles.LoginButton}>
-                <Button title="Login" onPress={handleLogin} />
+            {errMessage && <ErrorMessage errorMessage={errMessage} marginVertical={15} />}
+            <PrimaryButton text="Sign In" action={handleLogin} />
+            <View style={styles.extraOptions}>
+                <TextLink text="Create Account" redirectTo="/SignUp" />
+                <TextLinkIcon
+                    text="Forgot Password?"
+                    redirectTo="/PasswordReset"
+                    icon={<MaterialCommunityIcons name="lock-outline" size={17} color="#003580" />}
+                />
             </View>
-            <View style={styles.errorContainer}>
-                <Text style={styles.errorText}>{errMessage}</Text>
+            <View style={styles.dividerContainer}>
+                <View style={styles.divider}></View>
+                <Text style={styles.dividerText}>or</Text>
+                <View style={styles.divider}></View>
             </View>
-            <View style={styles.footerView}>
-                <Text style={styles.footerText}>
-                    Don't have an account? <Text> </Text>
-                    <Link to={"/SignUp"} style={styles.footerLink}>
-                        <Text>Sign up</Text>
-                    </Link>
-                </Text>
-            </View>
+            <GoogleAuth />
             {/* <GoogleAuth setUserGoogleLogin={setUserGoogleLogin} /> */}
+            <UserAgreement status="signing in" />
         </KeyboardAvoidingView>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 50,
-    },
-    logoContainer: {
-        padding: 35,
-        backgroundColor: "#003580",
-        fontFamily: "Poppins-Bold",
-        fontSize: 30,
-        fontWeight: "bold",
-        textAlign: "center",
-    },
-    logoHeading: {
-        fontSize: 30,
-        fontWeight: "bold",
-        textAlign: "center",
-        color: "#fff",
-    },
-    email: {
-        height: 40,
-        borderColor: "gray",
-        borderWidth: 1,
-        marginTop: 20,
-        padding: 10,
-    },
-    password: {
-        height: 40,
-        borderColor: "gray",
-        borderWidth: 1,
-        marginTop: 20,
-        padding: 10,
-    },
-    LoginButton: {
+    extraOptions: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
         marginTop: 20,
     },
-    errorText: {
-        color: "red",
-    },
-    errorContainer: {
-        height: 20,
-    },
-    footerView: {
+
+    dividerContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
         alignItems: "center",
-        marginTop: 20,
+        marginTop: 5,
     },
-    footerText: {
-        fontSize: 14,
-        color: "#2e2e2d",
+    divider: {
+        borderBottomColor: "#000",
+        borderBottomWidth: 1,
+        marginHorizontal: 20,
+        width: "35%",
     },
-    footerLink: {
-        color: "#788eec",
-        fontWeight: "bold",
-        fontSize: 16,
+    dividerText: {
+        textAlign: "center",
+        fontSize: 18,
+        fontWeight: "500",
+        color: "#000",
     },
 });
 
