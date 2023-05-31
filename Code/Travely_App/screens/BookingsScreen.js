2fc42dcd4d951d26d5aff5815393b7f81be8266b
gulraiz <gulraiznoorbari@gmail.com>
2023-06-01 01:16:19 +0500

    📦 NEW: Added Bookings Screen (template)

---
+++ Code/Travely_App/screens/BookingsScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const BookingsScreen = () => {
+    return (
+        <View>
+            <Text>BookingsScreen</Text>
+        </View>
+    );
+};
+
+export default BookingsScreen;
+
+const styles = StyleSheet.create({});
