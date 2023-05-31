c04cf500f36f222c789b701f47499484cc48676b
gulraiz <gulraiznoorbari@gmail.com>
2023-06-01 01:15:56 +0500

    📦 NEW: Added Wishlist Screen (template)

---
+++ Code/Travely_App/screens/WishListScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const WishListScreen = () => {
+    return (
+        <View>
+            <Text>WishlistScreen</Text>
+        </View>
+    );
+};
+
+export default WishListScreen;
+
+const styles = StyleSheet.create({});
