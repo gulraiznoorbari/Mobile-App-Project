b55778097512bafe0f9a3223b43e5b1b34c33943
gulraiz <gulraiznoorbari@gmail.com>
2023-06-03 17:52:51 +0500

    📦 NEW: Added Square Button component

---
+++ Code/Travely_App/components/Buttons/SquaredButton.js
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import FontLoader from "../FontLoader";
+
+const SquaredButton = ({ text, action, marginTop, padding }) => {
+    return (
+        <FontLoader>
+            <TouchableOpacity
+                style={{
+                    backgroundColor: "#003580",
+                    padding: padding,
+                    marginTop: marginTop,
+                    marginHorizontal: 20,
+                    borderRadius: 10,
+                    borderWidth: 1,
+                    borderColor: "#000",
+                }}
+                onPress={action}
+            >
+                <Text
+                    style={{
+                        fontFamily: "Poppins SemiBold",
+                        fontSize: 16,
+                        textAlign: "center",
+                        color: "#fff",
+                    }}
+                >
+                    {text}
+                </Text>
+            </TouchableOpacity>
+        </FontLoader>
+    );
+};
+
+export default SquaredButton;
